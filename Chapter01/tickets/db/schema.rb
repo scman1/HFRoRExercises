36bf873d8bd76b8b7c0ef2991dff220f8a4e0760
Abraham Nieva de la Hidalga <a_nieva@hotmail.com>
2019-02-12 12:41:52 +0000

    Add Phone, page 37
    Migratio, schema and four app files (3 views and one cotroller)

--- Chapter01/tickets/db/schema.rb
+++ Chapter01/tickets/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190206163624) do
+ActiveRecord::Schema.define(version: 20190212120000) do
 
   create_table "tickets", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20190206163624) do
     t.string "email_address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "phone"
   end
 
 end
